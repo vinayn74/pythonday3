b32d0c9ed12c0d5567323b67d9773b5badadc181
Vinay <rajvinay9067@gmail.com>
2026-02-04 10:30:23 +0530

    day 3 inheritance

---
+++ class.py
@@ -0,0 +1,23 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    def display_book_details(self):
+        print("Title :", self.title)
+        print("Author :", self.author)
+class IssuedBook(Book):
+    def __init__(self, title, author, issued_to, issued_date):
+        super().__init__(title, author)
+        self.issued_to = issued_to
+        self.issued_date = issued_date
+    def display_book_details(self):
+        super().display_book_details()
+    def display_issued_book_details(self):
+        super().display_book_details()
+        print("Issued To :", self.issued_to)
+        print("Issued Date :", self.issued_date)
+i_b = IssuedBook("OOP's in Python", "Bhaskar", "Vinay", "04-06-2026")
+i_b.display_issued_book_details()
+
+
+        
